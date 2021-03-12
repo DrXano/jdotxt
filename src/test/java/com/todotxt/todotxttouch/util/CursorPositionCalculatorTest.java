fab0897a6ec1c22d8d0b583d41a2e953d63586d0
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2021-03-12 15:53:12 +0000

    Uma função concluida

--- src/test/java/com/todotxt/todotxttouch/util/CursorPositionCalculatorTest.java
+++ src/test/java/com/todotxt/todotxttouch/util/CursorPositionCalculatorTest.java
@@ -2,9 +2,19 @@ package com.todotxt.todotxttouch.util;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CursorPositionCalculatorTest {
+	
+	private String priorValue;
+	private String newValue;
+	
+	@Before
+	public void setUp() {
+		priorValue = "qwe";
+		newValue = "qwerty";
+	}
 
 	@Test
 	/*
@@ -114,6 +124,116 @@ public class CursorPositionCalculatorTest {
 		String priorValue = "qwerty";
 		String newValue = "";
 		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+		int expected = 0;
+
+		assertEquals(expected, actual);
+	}
+	
+	//-----------------------------------Boundary Value Analysis--------------------------------------
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void AtestA() {
+		int priorCursorPosition = 0;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+		int expected = 3;
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void BtestB() {
+		int priorCursorPosition = 2;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+		int expected = 5;
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void CtestC() {
+		int priorCursorPosition = -1;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+		int expected = 3;
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void DtestD() {
+		int priorCursorPosition = 3;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+		int expected = 6;
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void EtestE() {
+		int priorCursorPosition = -3;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+		int expected = 3;
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void FtestF() {
+		int priorCursorPosition = 1;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+		int expected = 4;
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void GtestG() {
+		int priorCursorPosition = 5;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+		int expected = 6;
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void HtestH() {
+		int priorCursorPosition = 1;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, null, newValue);
+		int expected = 6;
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/*
+	 * Method - calculate
+	 */
+	public void ItestI() {
+		int priorCursorPosition = 1;
+		int actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, null);
 		int expected = 0;
 
 		assertEquals(expected, actual);
