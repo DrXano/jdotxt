49cf341851d2733456e08b9b035d147f9b30de50
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2021-03-26 22:33:24 +0000

    Mais algum coverage

---
+++ src/test/java/com/todotxt/todotxttouch/task/LocalFileTaskRepositoryTest.java
@@ -0,0 +1,104 @@
+package com.todotxt.todotxttouch.task;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LocalFileTaskRepositoryTest {
+
+	private ArrayList<Task> savedtodotasks;
+	private ArrayList<Task> saveddonetasks;
+	private LocalFileTaskRepository repo;
+	
+	@Before
+	public void setUp() throws Exception {
+		repo = new LocalFileTaskRepository();
+		savedtodotasks = repo.load();
+		saveddonetasks = repo.loadDoneTasks();
+		repo.purge();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		repo.store(savedtodotasks);
+		repo.storeDoneTasks(saveddonetasks);
+	}
+
+	@Test
+	public void testStoredTodoTasks() {
+		ArrayList<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(1, "task1"));
+		tasks.add(new Task(2, "task2"));
+		tasks.add(new Task(3, "task3"));
+		repo.store(tasks);
+		ArrayList<Task> expected = tasks;
+		ArrayList<Task> actual = repo.load();
+		repo.purge();
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testStoredDoneTasks() {
+		ArrayList<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(1, "task1"));
+		tasks.add(new Task(2, "task2"));
+		tasks.add(new Task(3, "task3"));
+		repo.storeDoneTasks(tasks);
+		ArrayList<Task> expected = tasks;
+		ArrayList<Task> actual = repo.loadDoneTasks();
+		repo.purge();
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void testArchivedTodoTasks() {
+		ArrayList<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(1, "task1"));
+		tasks.add(new Task(2, "task2"));
+		tasks.add(new Task(3, "task3"));
+		tasks.get(0).markComplete(new Date());
+		tasks.get(1).markIncomplete();
+		tasks.get(2).markComplete(new Date());
+		repo.archive(tasks);
+		ArrayList<Task> loaded = repo.load();
+		assertEquals(tasks.get(1),loaded.get(0));
+	}
+	
+	@Test
+	public void testArchivedDoneTasks() {
+		ArrayList<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(1, "task1"));
+		tasks.add(new Task(2, "task2"));
+		tasks.add(new Task(3, "task3"));
+		tasks.get(0).markComplete(new Date());
+		tasks.get(1).markIncomplete();
+		tasks.get(2).markIncomplete();
+		repo.archive(tasks);
+		ArrayList<Task> loaded = repo.loadDoneTasks();
+		assertEquals(tasks.get(0),loaded.get(0));
+	}
+	
+	@Test
+	public void testTodoFileModifiedSince() {
+		LocalDate ld = LocalDate.of(2020, 1, 1);
+		Date d = Date.from(ld.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		assertFalse(repo.todoFileModifiedSince(d));
+	}
+	
+	@Test
+	public void testDoneFileModifiedSince() {
+		LocalDate ld = LocalDate.of(2020, 1, 1);
+		Date d = Date.from(ld.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		assertTrue(repo.doneFileModifiedSince(d));
+	}
+
+}
