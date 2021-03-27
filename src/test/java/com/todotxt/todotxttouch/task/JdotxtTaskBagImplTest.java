843ca9a9db28afb9d89e1c815944fde286a3bf7d
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2021-03-27 18:40:47 +0000

    A tentar aumentar o coverage

--- src/test/java/com/todotxt/todotxttouch/task/JdotxtTaskBagImplTest.java
+++ src/test/java/com/todotxt/todotxttouch/task/JdotxtTaskBagImplTest.java
@@ -1,10 +1,22 @@
 package com.todotxt.todotxttouch.task;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.prefs.Preferences;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.chschmid.jdotxt.Jdotxt;
+import com.chschmid.jdotxt.gui.JdotxtGUI;
+import com.chschmid.jdotxt.util.LanguagesController;
 
 public class JdotxtTaskBagImplTest {
 
@@ -15,6 +27,10 @@ public class JdotxtTaskBagImplTest {
 	
 	@Before
 	public void setUp() throws Exception {
+		
+		Preferences userPrefs = Preferences.userNodeForPackage(Jdotxt.class);
+		JdotxtGUI.lang = new LanguagesController(userPrefs.get("lang", "English"));
+		
 		repo = new LocalFileTaskRepository();
 		savedtodotasks = repo.load();
 		saveddonetasks = repo.loadDoneTasks();
@@ -31,8 +47,329 @@ public class JdotxtTaskBagImplTest {
 
 	@Test
 	public void testBagAddAsTask() {
-		bag.addAsTask("somaTask");
+		List<Task> expected = new ArrayList<>();
+		expected.add(new Task(0, "someTask", new Date()));
+		expected.add(new Task(1, "anyTask", new Date()));
+		expected.add(new Task(2, "noTask", new Date()));
+		
+		bag.addAsTask(expected.get(0).inFileFormat());
+		bag.addAsTask(expected.get(1).inFileFormat());
+		bag.addAsTask(expected.get(2).inFileFormat());
+		List<Task> actual = bag.getTasks();
+		assertEquals(expected,actual);
 		bag.clear();
 	}
+	
+	@Test
+	public void testBagUpdate1() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask", new Date()));
+		tasks.add(new Task(1, "anyTask", new Date()));
+		tasks.add(new Task(2, "noTask", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+		
+		Date dt = new Date();
+		Calendar c = Calendar.getInstance(); 
+		c.setTime(dt); 
+		c.add(Calendar.DATE, 1);
+		dt = c.getTime();
+		
+		Task expected = new Task(1, "anyTask", dt);
+		
+		bag.update(expected);
+		List<Task> updatedTasks = bag.getTasks();
+		
+		assertEquals(expected, updatedTasks.get(1));
+	}
+	
+	@Test
+	public void testBagUpdate2() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask", new Date()));
+		tasks.add(new Task(1, "anyTask", new Date()));
+		tasks.add(new Task(2, "noTask", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+		
+		bag.update(null);
+		
+		assertEquals(tasks,bag.getTasks());
+	}
+	
+	@Test(expected = TaskPersistException.class)
+	public void testBagUpdate3() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask", new Date()));
+		tasks.add(new Task(1, "anyTask", new Date()));
+		tasks.add(new Task(2, "noTask", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
 
+		Task updatedTask = new Task(3, "voidTask", new Date());
+		bag.update(updatedTask);
+	}
+	
+	@Test
+	public void testBagDelete1() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask", new Date()));
+		tasks.add(new Task(1, "anyTask", new Date()));
+		tasks.add(new Task(2, "noTask", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+		
+		Task toRemove = tasks.get(1);
+		bag.delete(toRemove);
+		tasks.remove(toRemove);
+		
+		assertEquals(tasks, bag.getTasks());
+	}
+	
+	@Test(expected = TaskPersistException.class)
+	public void testBagDelete2() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask", new Date()));
+		tasks.add(new Task(1, "anyTask", new Date()));
+		tasks.add(new Task(2, "noTask", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+		
+		Task toRemove = new Task(3, "voidTask", new Date());
+		bag.delete(toRemove);
+	}
+	
+	@Test
+	public void testBagGetPriorities() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask", new Date()));
+		tasks.add(new Task(1, "anyTask", new Date()));
+		tasks.add(new Task(2, "noTask", new Date()));
+		
+		tasks.get(0).setPriority(Priority.A);
+		tasks.get(1).setPriority(Priority.D);
+		tasks.get(2).setPriority(Priority.E);
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+		
+		ArrayList<Priority> expected = new ArrayList<>();
+		expected.add(Priority.A);
+		expected.add(Priority.D);
+		expected.add(Priority.E);
+		
+		assertEquals(expected,bag.getPriorities());
+	}
+	
+	@Test
+	public void testBagGetContexts1() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask @Cont1 @Cont2", new Date()));
+		tasks.add(new Task(1, "anyTask @Cont3", new Date()));
+		tasks.add(new Task(2, "noTask @Cont4", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+		
+		ArrayList<String> expected = new ArrayList<>();
+		expected.add("Cont1");
+		expected.add("Cont2");
+		expected.add("Cont3");
+		expected.add("Cont4");
+		assertEquals(expected,bag.getContexts(false));
+	}
+	
+	@Test
+	public void testBagGetContexts2() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask @Cont1 @Cont2", new Date()));
+		tasks.add(new Task(1, "anyTask @Cont3", new Date()));
+		tasks.add(new Task(2, "noTask @Cont4", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+		
+		ArrayList<String> expected = new ArrayList<>();
+		expected.add("-");
+		expected.add("Cont1");
+		expected.add("Cont2");
+		expected.add("Cont3");
+		expected.add("Cont4");
+		assertEquals(expected,bag.getContexts(true));
+	}
+	
+	@Test
+	public void testBagGetProjects1() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask @Cont1 @Cont2 +Proj1", new Date()));
+		tasks.add(new Task(1, "anyTask @Cont3 +Proj2", new Date()));
+		tasks.add(new Task(2, "noTask @Cont4 +Proj3 +Proj4", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+
+		ArrayList<String> expected = new ArrayList<>();
+		expected.add("Proj1");
+		expected.add("Proj2");
+		expected.add("Proj3");
+		expected.add("Proj4");
+		assertEquals(expected,bag.getProjects(false));
+	}
+	
+	@Test
+	public void testBagGetProjects2() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask @Cont1 @Cont2 +Proj1", new Date()));
+		tasks.add(new Task(1, "anyTask @Cont3 +Proj2", new Date()));
+		tasks.add(new Task(2, "noTask @Cont4 +Proj3 +Proj4", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+
+		ArrayList<String> expected = new ArrayList<>();
+		expected.add("-");
+		expected.add("Proj1");
+		expected.add("Proj2");
+		expected.add("Proj3");
+		expected.add("Proj4");
+		assertEquals(expected,bag.getProjects(true));
+	}
+	
+	@Test
+	public void testBagSize() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(0, "someTask", new Date()));
+		tasks.add(new Task(1, "anyTask", new Date()));
+		tasks.add(new Task(2, "noTask", new Date()));
+		
+		for(Task t : tasks)
+			bag.addAsTask(t.inFileFormat());
+		
+		assertTrue(bag.size() == 3);
+	}
+	
+	@Test
+	public void testBagStore() {
+		List<Task> expected = new ArrayList<>();
+		expected.add(new Task(0, "someTask", new Date()));
+		expected.add(new Task(1, "anyTask", new Date()));
+		expected.add(new Task(2, "noTask", new Date()));
+		
+		for(Task t : expected)
+			bag.addAsTask(t.inFileFormat());
+		
+		bag.store();
+		bag.reload();
+		
+		List<Task> actual = bag.getTasks();
+		
+		assertEquals(expected, actual);
+		bag.clear();
+	}
+	
+	@Test
+	public void testBagArchive1() {
+		List<Task> expected = new ArrayList<>();
+		Task t1 = new Task(0, "someTask", new Date());
+		Task t2 = new Task(1, "anyTask", new Date());
+		Task t3 = new Task(2, "noTask", new Date());
+		
+		t2.markComplete(new Date());
+		
+		expected.add(t1);
+		expected.add(t2);
+		expected.add(t3);
+		
+		for(Task t : expected)
+			bag.addAsTask(t.inFileFormat());
+		
+		bag.archive();
+		bag.reload();
+		List<Task> actual = bag.getTasks();
+		assertFalse(actual.contains(t2));
+		bag.clear();
+	}
+	
+	@Test
+	public void testBagArchive2() {
+		List<Task> expected = new ArrayList<>();
+		Task t1 = new Task(0, "someTask", new Date());
+		Task t2 = new Task(1, "anyTask", new Date());
+		Task t3 = new Task(2, "noTask", new Date());
+		
+		t1.markComplete(new Date());
+		t2.markComplete(new Date());
+		
+		expected.add(t1);
+		expected.add(t2);
+		expected.add(t3);
+		
+		for(Task t : expected)
+			bag.addAsTask(t.inFileFormat());
+		
+
+		bag.archive();
+		bag.unarchive(t1);
+		bag.reload();
+		List<Task> actual = bag.getTasks();
+		assertTrue(actual.contains(t1));
+		bag.clear();
+	}
+	
+	@Test
+	public void testBagArchive3() {
+		List<Task> expected = new ArrayList<>();
+		Task t1 = new Task(0, "someTask", new Date());
+		Task t2 = new Task(1, "anyTask", new Date());
+		Task t3 = new Task(2, "noTask", new Date());
+		
+		t1.markComplete(new Date());
+		t2.markComplete(new Date());
+		
+		expected.add(t1);
+		expected.add(t2);
+		expected.add(t3);
+		
+		for(Task t : expected)
+			bag.addAsTask(t.inFileFormat());
+		
+
+		bag.archive();
+		bag.unarchive(new Task(-1, "someTask", new Date()));
+		bag.reload();
+		List<Task> actual = bag.getTasks();
+		assertTrue(actual.contains(t1));
+		bag.clear();
+	}
+	
+	@Test
+	public void testBagArchive4() {
+		List<Task> expected = new ArrayList<>();
+		Task t1 = new Task(0, "someTask", new Date());
+		Task t2 = new Task(1, "anyTask", new Date());
+		Task t3 = new Task(2, "noTask", new Date());
+		
+		t2.markComplete(new Date());
+		t3.markComplete(new Date());
+		
+		expected.add(t1);
+		expected.add(t2);
+		expected.add(t3);
+		
+		for(Task t : expected)
+			bag.addAsTask(t.inFileFormat());
+		
+
+		bag.archive();
+		bag.unarchive(new Task(5, "noTask", new Date()));
+		bag.reload();
+		List<Task> actual = bag.getTasks();
+		assertTrue(actual.contains(t3));
+		bag.clear();
+	}
 }
---
+++ src/test/java/com/todotxt/todotxttouch/util/TreeTest.java
@@ -0,0 +1,100 @@
+package com.todotxt.todotxttouch.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TreeTest {
+
+	@Test
+	public void testIsLoaded1() {
+		Tree<Integer> tree = new Tree<>(2);
+		assertFalse(tree.isLoaded());
+	}
+	
+	@Test
+	public void testIsLoaded2() {
+		Tree<Integer> tree = new Tree<>(2);
+		tree.setLoaded();
+		assertTrue(tree.isLoaded());
+	}
+	
+	@Test
+	public void testGetData() {
+		Tree<String> tree = new Tree<>("Olá");
+		assertEquals("Olá",tree.getData());
+	}
+	
+	@Test
+	public void testGetChildrenNull() {
+		Tree<String> tree = new Tree<>("Olá");
+		assertNull(tree.getChildren());
+	}
+	
+	@Test
+	public void testGetParent() {
+		Tree<String> parent = new Tree<>("Olá");
+		Tree<String> child = parent.addChild("Adeus");
+		assertEquals(parent,child.getParent());
+	}
+	
+	@Test
+	public void testContainsChild1() {
+		Tree<String> parent = new Tree<>("Olá");
+		Tree<String> child = new Tree<>(parent, "Adeus");
+		parent.addChild(child);
+		assertTrue(parent.contains(child));
+	}
+	
+	@Test
+	public void testContainsChild2() {
+		Tree<String> parent = new Tree<>("Olá");
+		Tree<String> child = new Tree<>(parent, "Adeus");
+		assertFalse(parent.contains(child));
+	}
+	
+	@Test
+	public void testContainsChild3() {
+		Tree<String> parent = new Tree<>("Olá");
+		Tree<String> child = new Tree<>(parent, "Adeus");
+		parent.setLoaded();
+		assertFalse(parent.contains(child));
+	}
+	
+	@Test
+	public void testContainsData1() {
+		Tree<String> parent = new Tree<>("Olá");
+		assertFalse(parent.contains("Adeus"));
+	}
+	
+	@Test
+	public void testContainsData2() {
+		Tree<String> parent = new Tree<>("Olá");
+		parent.setLoaded();
+		assertFalse(parent.contains("Adeus"));
+	}
+	
+	@Test
+	public void testContainsData3() {
+		Tree<String> parent = new Tree<>("Olá");
+		parent.setLoaded();
+		parent.addChild("Ok");
+		parent.addChild("sim");
+		parent.addChild("nao");
+		assertTrue(parent.contains("sim"));
+	}
+	
+	@Test
+	public void testGetChild() {
+		Tree<String> parent = new Tree<>("Olá");
+		Tree<String> expected = new Tree<>("sim");
+		parent.addChild("Ok");
+		parent.addChild(expected);
+		parent.addChild("nao");
+		assertEquals(expected,parent.getChild(1));
+	}
+
+}
