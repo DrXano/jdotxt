b2a62de2570e59ee6f47d269c0e95ec8862adcc6
João Afonso <johnlandon1999@gmail.com>
2021-04-24 12:26:18 +0100

    RelativeDateTest feito

--- src/test/java/com/todotxt/todotxttouch/util/RelativeDateTest.java
+++ src/test/java/com/todotxt/todotxttouch/util/RelativeDateTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.prefs.Preferences;
 
@@ -92,6 +93,52 @@ public class RelativeDateTest {
 		Calendar converted = GregorianCalendar.getInstance();
 		converted.setTime(new SimpleDateFormat("yyyy-MM-dd").parse(date));
 		return converted;
+	}
+	
+	//Mutantes
+	
+	@Test
+	public void testRelativeDateToday() throws ParseException {
+		String expected = "today";
+		assertEquals(expected, RelativeDate.getRelativeDate(today, getCalendarOf("2020-08-12")));
+	}
+	
+	@Test
+	public void testRelativeDate2DaysAgo() throws ParseException {
+		String expected = "2 days ago";
+		assertEquals(expected, RelativeDate.getRelativeDate(today, getCalendarOf("2020-08-10")));
+	}
+	
+	@Test
+	public void testRelativeDate1MonthAgo30Days() throws ParseException {
+		String expected = "1 month ago";
+		assertEquals(expected, RelativeDate.getRelativeDate(today, getCalendarOf("2020-07-13")));
+	}
+	
+	@Test
+	public void testRelativeDate2MonthsAgo60Days() throws ParseException {
+		String expected = "2 months ago";
+		assertEquals(expected, RelativeDate.getRelativeDate(today, getCalendarOf("2020-06-13")));
+	}
+	
+	@Test
+	public void testRelativeDateFuture() throws ParseException {
+		String expected = "2021-08-12";
+		assertEquals(expected, RelativeDate.getRelativeDate(today, getCalendarOf("2021-08-12")));
+	}
+	
+	@Test
+	public void testRelativeDateFarInThePast() throws ParseException {
+		String expected = "2019-08-13";
+		assertEquals(expected, RelativeDate.getRelativeDate(today, getCalendarOf("2019-08-13")));
+	}
+	
+	@Test
+	public void testRelativeDateWithDate() throws ParseException {
+		String expected = "1999-08-12";
+		Calendar testDay = getCalendarOf("1999-08-12");
+		Date date = testDay.getTime();
+		assertEquals(expected, RelativeDate.getRelativeDate(date));
 	}
 
 }
